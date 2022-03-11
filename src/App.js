2a616f05424a3625fecc91b2c0229643366f7499
AlexAlcar <cem20601@gmail.com>
2022-03-11 18:18:19 +0100

    primer layout

--- src/App.js
+++ src/App.js
@@ -4,13 +4,36 @@ import "primeicons/primeicons.css";                                //icons
 import './App.css';
 import { Card } from 'primereact/card';
 import FormularioRegistro from './components/FormularioRegistro';
+import { Splitter, SplitterPanel } from 'primereact/splitter';
 
 function App() {
   return (
     <div className="App">
-      <Card title="Formulario de registro" style={{ width: '55rem', marginBottom: '2em' }}>
-        <FormularioRegistro />
-      </Card>
+      
+      <Splitter style={{ height: '300px' }}>
+        <SplitterPanel className="flex align-items-center justify-content-center" size={20} minSize={10}>
+          Panel 1
+        </SplitterPanel>
+        <SplitterPanel size={80}>
+          <Splitter layout="vertical">
+            <SplitterPanel className="flex align-items-center justify-content-center" size={15}>
+              Panel 2
+            </SplitterPanel>
+            <SplitterPanel size={85}>
+              <Splitter>
+                <SplitterPanel className="flex align-items-center justify-content-center" size={20}>
+                  Panel 3
+                </SplitterPanel>
+                <SplitterPanel className="flex align-items-center justify-content-center" size={80}>
+                  Panel 4
+                </SplitterPanel>
+              </Splitter>
+            </SplitterPanel>
+          </Splitter>
+        </SplitterPanel>
+      </Splitter>
+
+      
     </div>
   );
 }
--- src/components/FormularioRegistro.jsx
+++ src/components/FormularioRegistro.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
 import { Dropdown } from 'primereact/dropdown';
-import { Calendar } from 'primereact/calendar';
 import { Password } from 'primereact/password';
 import { Checkbox } from 'primereact/checkbox';
 import { Dialog } from 'primereact/dialog';
@@ -30,7 +30,7 @@ const FormularioRegistro = () => {
 
     useEffect(() => {
 
-    }, []); 
+    }, []);
 
     const { control, formState: { errors }, handleSubmit, reset } = useForm({ defaultValues });
 
@@ -62,105 +62,108 @@ const FormularioRegistro = () => {
     );
 
     return (
-        <div className="form-demo">
-            <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
-                <div className="flex justify-content-center flex-column pt-6 px-3">
-                    <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
-                    <h5>Registration Successful!</h5>
-                    <p style={{ lineHeight: 1.5, textIndent: '1rem' }}>
-                        <p>Tu cuenta se ha registrado a nombre de <b>{formData.name}</b> . Será válido durante 30 días sin activación.</p> 
-                        Por favor revisa en <b>{formData.email}</b> las instrucciones de activación.
-                    </p>
-                </div>
-            </Dialog>
+        <Card title="Formulario de registro" style={{ width: '55rem', marginBottom: '2em' }}>
+
+            <div className="form-demo">
+                <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
+                    <div className="flex justify-content-center flex-column pt-6 px-3">
+                        <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
+                        <h5>Registration Successful!</h5>
+                        <p style={{ lineHeight: 1.5, textIndent: '1rem' }}>
+                            <p>Tu cuenta se ha registrado a nombre de <b>{formData.name}</b> . Será válido durante 30 días sin activación.</p>
+                            Por favor revisa en <b>{formData.email}</b> las instrucciones de activación.
+                        </p>
+                    </div>
+                </Dialog>
 
 
-            <div className="flex justify-content-center">
-                <div className="card">
-                    
-                    <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
-                        <div className="field">
-                            <span className="p-float-label">
-                                <Controller name="nombre" control={control} rules={{ required: 'Debes introducir un nombre' }} render={({ field, fieldState }) => (
-                                    <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
-                                )} />
-                                <label htmlFor="name" className={classNames({ 'p-error': errors.name })}>Nombre*</label>
-                            </span>
-                            {getFormErrorMessage('name')}
-                        </div>
-                        <div className="field">
-                            <span className="p-float-label">
-                                <Controller name="apellido1" control={control} rules={{ required: 'Debes introducir el primer apellido.' }} render={({ field, fieldState }) => (
-                                    <InputText id={field.apellido1} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
-                                )} />
-                                <label htmlFor="apellido1" className={classNames({ 'p-error': errors.name })}>Primer Apellido*</label>
-                            </span>
-                            {getFormErrorMessage('name')}
-                        </div>
-                        <div className="field">
-                            <span className="p-float-label">
-                                <Controller name="apellido2" control={control} render={({ field, fieldState }) => (
-                                    <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
-                                )} />
-                                <label htmlFor="apellido2" className={classNames({ 'p-error': errors.name })}>Segundo Apellido</label>
-                            </span>
-                            {getFormErrorMessage('name')}
-                        </div>
-                        <div className="field">
-                            <span className="p-float-label p-input-icon-right">
-                                <i className="pi pi-envelope" />
-                                <Controller name="email" control={control}
-                                    rules={{ required: 'Debes introducir una dirección de correo electrónico.', pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: 'Introduce una dirección válida. Ej. example@email.com' } }}
-                                    render={({ field, fieldState }) => (
-                                        <InputText id={field.name} {...field} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                <div className="flex justify-content-center">
+                    <div className="card">
+
+                        <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller name="nombre" control={control} rules={{ required: 'Debes introducir un nombre' }} render={({ field, fieldState }) => (
+                                        <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
                                     )} />
-                                <label htmlFor="email" className={classNames({ 'p-error': !!errors.email })}>Email*</label>
-                            </span>
-                            {getFormErrorMessage('email')}
-                        </div>
-                        <div className="field">
-                            <span className="p-float-label">
-                                <Controller name="direccion" control={control} rules={{ required: 'Debes introducir la dirección.' }} render={({ field, fieldState }) => (
-                                    <InputText id={field.direccion} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
-                                )} />
-                                <label htmlFor="direccion" className={classNames({ 'p-error': errors.name })}>Dirección*</label>
-                            </span>
-                            {getFormErrorMessage('name')}
-                        </div>
-                        <Divider align="center"><p>Datos de usuario</p></Divider>
-                        <div className="field" style={{paddding:'5px'}}>
-                            <span className="p-float-label">
-                                <Controller name="usuario" control={control} rules={{ required: 'Debes un nombre de usuario.' }} render={({ field, fieldState }) => (
-                                    <InputText id={field.usuario} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
-                                )} />
-                                <label htmlFor="usuario" className={classNames({ 'p-error': errors.name })}>Nombre de usuario*</label>
-                            </span>
-                            {getFormErrorMessage('name')}
-                        </div>
-                        <div className="field">
-                            <span className="p-float-label">
-                                <Controller name="password" control={control} rules={{ required: 'Debes introducir una contraseña.' }} render={({ field, fieldState }) => (
-                                    <Password id={field.name} {...field} toggleMask className={classNames({ 'p-invalid': fieldState.invalid })} header={passwordHeader} footer={passwordFooter} />
-                                )} />
-                                <label htmlFor="password" className={classNames({ 'p-error': errors.password })}>Password*</label>
-                            </span>
-                            {getFormErrorMessage('password')}
-                        </div>
-                        
-                        
-                        
-                        <div className="field-checkbox">
-                            <Controller name="accept" control={control} rules={{ required: true }} render={({ field, fieldState }) => (
-                                <Checkbox inputId={field.name} onChange={(e) => field.onChange(e.checked)} checked={field.value} className={classNames({ 'p-invalid': fieldState.invalid })} />
-                            )} />
-                            <label htmlFor="accept" className={classNames({ 'p-error': errors.accept })}> Acepto los términos y condiciones*</label>
-                        </div>&nbsp;
+                                    <label htmlFor="name" className={classNames({ 'p-error': errors.name })}>Nombre*</label>
+                                </span>
+                                {getFormErrorMessage('name')}
+                            </div>
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller name="apellido1" control={control} rules={{ required: 'Debes introducir el primer apellido.' }} render={({ field, fieldState }) => (
+                                        <InputText id={field.apellido1} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                    )} />
+                                    <label htmlFor="apellido1" className={classNames({ 'p-error': errors.name })}>Primer Apellido*</label>
+                                </span>
+                                {getFormErrorMessage('name')}
+                            </div>
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller name="apellido2" control={control} render={({ field, fieldState }) => (
+                                        <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                    )} />
+                                    <label htmlFor="apellido2" className={classNames({ 'p-error': errors.name })}>Segundo Apellido</label>
+                                </span>
+                                {getFormErrorMessage('name')}
+                            </div>
+                            <div className="field">
+                                <span className="p-float-label p-input-icon-right">
+                                    <i className="pi pi-envelope" />
+                                    <Controller name="email" control={control}
+                                        rules={{ required: 'Debes introducir una dirección de correo electrónico.', pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: 'Introduce una dirección válida. Ej. example@email.com' } }}
+                                        render={({ field, fieldState }) => (
+                                            <InputText id={field.name} {...field} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                        )} />
+                                    <label htmlFor="email" className={classNames({ 'p-error': !!errors.email })}>Email*</label>
+                                </span>
+                                {getFormErrorMessage('email')}
+                            </div>
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller name="direccion" control={control} rules={{ required: 'Debes introducir la dirección.' }} render={({ field, fieldState }) => (
+                                        <InputText id={field.direccion} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                    )} />
+                                    <label htmlFor="direccion" className={classNames({ 'p-error': errors.name })}>Dirección*</label>
+                                </span>
+                                {getFormErrorMessage('name')}
+                            </div>
+                            <Divider align="center"><p>Datos de usuario</p></Divider>
+                            <div className="field" style={{ paddding: '5px' }}>
+                                <span className="p-float-label">
+                                    <Controller name="usuario" control={control} rules={{ required: 'Debes un nombre de usuario.' }} render={({ field, fieldState }) => (
+                                        <InputText id={field.usuario} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                    )} />
+                                    <label htmlFor="usuario" className={classNames({ 'p-error': errors.name })}>Nombre de usuario*</label>
+                                </span>
+                                {getFormErrorMessage('name')}
+                            </div>
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller name="password" control={control} rules={{ required: 'Debes introducir una contraseña.' }} render={({ field, fieldState }) => (
+                                        <Password id={field.name} {...field} toggleMask className={classNames({ 'p-invalid': fieldState.invalid })} header={passwordHeader} footer={passwordFooter} />
+                                    )} />
+                                    <label htmlFor="password" className={classNames({ 'p-error': errors.password })}>Password*</label>
+                                </span>
+                                {getFormErrorMessage('password')}
+                            </div>
 
-                        <Button type="submit" label="Submit" className="mt-2" />
-                    </form>
+
+
+                            <div className="field-checkbox">
+                                <Controller name="accept" control={control} rules={{ required: true }} render={({ field, fieldState }) => (
+                                    <Checkbox inputId={field.name} onChange={(e) => field.onChange(e.checked)} checked={field.value} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                                <label htmlFor="accept" className={classNames({ 'p-error': errors.accept })}> Acepto los términos y condiciones*</label>
+                            </div>&nbsp;
+
+                            <Button type="submit" label="Submit" className="mt-2" />
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Card>
     );
 }
 
