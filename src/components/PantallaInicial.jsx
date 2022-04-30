07ba1055534ca40e495a951974c175f8df4b81b4
Alex Alba <cem20601@gmail.com>
2022-04-30 22:28:12 +0200

    Update PantallaInicial.jsx

--- src/components/PantallaInicial.jsx
+++ src/components/PantallaInicial.jsx
@@ -2,15 +2,18 @@ import { Navigate } from "react-router";
 import { Card } from 'primereact/card';
 import { Chart } from 'primereact/chart';
 import { Divider } from 'primereact/divider';
+import Http from "../Helpers/Http";
 import React, { useEffect, useState } from 'react';
 //import 'primeflex/primeflex.css';
 
 const PantallaInicial = ({ authorized }) => {
-    const [chartUsuarios] = useState({
+    //crear un array que será la fuente de datos en DATA
+    const[chartUserData, setChartUserData]=useState([]);
+    const [chartUsuarios, setChartUsuarios] = useState({
         labels: ['Usuarios totales', 'Solicitantes', 'Impresores'],
         datasets: [
             {
-                data: [300, 50, 100],
+                data: chartUserData,
                 backgroundColor: [
                     "#42A5F5",
                     "#66BB6A",
@@ -36,8 +39,7 @@ const PantallaInicial = ({ authorized }) => {
     });
 
 
-
-    const [chartPedidos] = useState({
+    const [chartPedidos, setChartPedidos] = useState({
         labels: ['Pedidos Totales', 'Pedidos en proceso', 'Pedidos entregados'],
         datasets: [
             {
@@ -64,6 +66,47 @@ const PantallaInicial = ({ authorized }) => {
             }
         }
     });
+
+    const getData=async()=>{
+        let usuariosTotales, conteoImpresores, conteoUsuarios;
+        let pedidosTotales, conteoPedidosEnProceso, conteoPedidosEntregados;
+        const resUsuarios = await Http.get("/api/usuarios/");
+        const resPedidos = await Http.get("/api/pedidos/");
+
+        //console.log(resPedidos.length);
+        setChartUsuarios({...chartUsuarios,
+        datasets:[
+            {
+                data: [resUsuarios.length,20,30],
+                backgroundColor: [
+                    "#42A5F5",
+                    "#66BB6A",
+                    "#FFA726"
+                ],
+                hoverBackgroundColor: [
+                    "#64B5F6",
+                    "#81C784",
+                    "#FFB74D"
+                ]
+            }
+        ]
+        })
+         
+        //setChartUserData(2, 100, 100);
+
+        //console.log(chartUserData);
+
+        //console.log(usuariosTotales)
+
+
+        
+    }
+
+
+    useEffect(() => {
+        getData();
+    }, []);
+
 
     return (
         <>
